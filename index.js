e1ca7ea2b19418f53f6595d97de3c4732e2f8b24
AaronSala <salaaron2@gmail.com>
2023-03-14 17:05:04 +0300

    function that outputs the largest number

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function maximum(){
+    let arrey=[3,8,2,0,81,23]
+    let max =arrey[0]
+    for (let i=0; i<arrey.length; i++){
+    
+    if (arrey[i]>max){
+        max=arrey[i];
+    }
+}
+    console.log(max);
+}
+maximum();
